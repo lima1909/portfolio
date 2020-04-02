5d87e9a7f4b83addd698da148bbaa6b33217989e
lima1909 <linke.mario@web.de>
2020-04-02 15:38:09 +0200

    remove datastructure for datastore lookup and create a JSON string

--- src/gcloud/datastore.rs
+++ src/gcloud/datastore.rs
@@ -1,67 +1,59 @@
-// use crate::gcloud::Store;
 use crate::gcloud::auth::Auth;
-// use futures::executor;
 use log::info;
-// use reqwest::blocking;
-use serde::{Deserialize, Serialize};
 
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct readOptions {
-    readConsistency: String,
-}
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct partitionId {
-    namespaceId: String,
-}
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct path {
-    kind: String,
-    id: String,
-}
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct key {
-    partitionId: partitionId,
-    path: path,
-}
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct lookup {
-    readOptions: readOptions,
-    keys: Vec<key>,
+const LOOKUP_JSON: &'static str = r#"{
+    "readOptions": { "readConsistency": "{readConsistency}" },
+    "keys": [
+      {
+        "partitionId": { "namespaceId": "{namespace}" },
+        "path": [
+          {
+            "kind": "{kind}",
+            "id": "{id}"
+          }
+        ]
+      }
+    ]
+}"#;
+
+enum ReadConsistency {
+    ReadConsistencyUnspecidied,
+    Strong,
+    Eventual,
 }
 
-fn create_lookup(namespace: String, kind: String, id: String) -> lookup {
-    lookup {
-        readOptions: readOptions {
-            readConsistency: "EVENTUAL".to_owned(),
-        },
-        keys: vec![key {
-            partitionId: partitionId {
-                namespaceId: namespace,
-            },
-            path: path { kind: kind, id: id },
-        }],
+impl ReadConsistency {
+    #[allow(dead_code)]
+    fn from_string(from: &str) -> Self {
+        match from {
+            "READ_CONSISTENCY_UNSPECIFIED" => ReadConsistency::ReadConsistencyUnspecidied,
+            "STRONG" => ReadConsistency::Strong,
+            _ => ReadConsistency::Eventual,
+        }
+    }
+
+    fn to_string(&self) -> &str {
+        match self {
+            ReadConsistency::ReadConsistencyUnspecidied => "READ_CONSISTENCY_UNSPECIFIED",
+            ReadConsistency::Strong => "STRONG",
+            ReadConsistency::Eventual => "EVENTUAL",
+        }
     }
 }
 
-impl lookup {
-    fn to_json(&self) -> String {
-        serde_json::to_string(self).unwrap()
-    }
+fn lookup_json(namespace: String, kind: String, id: String) -> String {
+    LOOKUP_JSON
+        .replace("{readConsistency}", ReadConsistency::Eventual.to_string())
+        .replace("{namespace}", &namespace)
+        .replace("{kind}", &kind)
+        .replace("{id}", &id)
+        .replace("\n", "")
 }
 
 pub struct Datastore<'a, T: Auth<'a>> {
     project: String,
     auth: &'a T,
+    client: reqwest::blocking::Client,
 }
 
 impl<'a, T> Datastore<'a, T>
@@ -72,18 +64,18 @@ where
         Datastore {
             project: project,
             auth: auth,
+            client: reqwest::blocking::Client::new(),
         }
     }
 
     pub fn lookup(self, namespace: String, kind: String, id: i128) {
-        let client = reqwest::blocking::Client::new();
         let url = format!(
             "https://datastore.googleapis.com/v1/projects/{}:lookup?{}",
             self.project,
             self.auth.to_query_url() // format!("access_token={}", at.access_token)
         );
-        let req_json = create_lookup(namespace, kind, id.to_string()).to_json();
-        let res = client.post(&url).body(req_json).send().unwrap();
+        let lookup_json = lookup_json(namespace, kind, id.to_string());
+        let res = self.client.post(&url).body(lookup_json).send().unwrap();
         info!("response status-code: {}", res.status());
         info!("response: {}", &res.text().unwrap()[..50]);
 
